dd85027f6697295a5e6cb1aff743fca4a0162492
azulay15 <azulay155@gmail.com>
2020-04-25 18:04:21 +0300

    adding title style

--- src/CommentList/CommentList.js
+++ src/CommentList/CommentList.js
@@ -5,7 +5,7 @@ import { Comment } from '../Comment/Comment';
  export function CommentList() {
     return (
         <div>
-            <h1>Article Comments</h1>
+            <h2>Article Comments</h2>
             <Comment author="Mor Azulay" date="24/04/2020 14:34" content="A new model by the Network Science Institute at Northeastern University in Boston shows the first infections came from China in early or mid-January, and that the virus went undetected because many people were not presenting symptoms."/>
             <Comment author="Bar Lary" date="24/04/2020 15:01" content="A new model by the Network Science Institute at Northeastern University in Boston shows the first infections came from China in early or mid-January, and that the virus went undetected because many people were not presenting symptoms."/>
             <Comment author="Aviv Halperin" date="25/04/2020 03:04" content="A new model by the Network Science Institute at Northeastern University in Boston shows the first infections came from China in early or mid-January, and that the virus went undetected because many people were not presenting symptoms."/>
